c16e3c2394a79d749ad85f1820486afa13e3e89e
angelfv93 <angelfv.93@icloud.com>
2021-09-16 09:30:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-zxgus3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-zxgus3)
---
+++ index.js
@@ -0,0 +1,5 @@
+// Import stylesheets
+import './style.css';
+
+const TextoBanner = document.getElementById('textoBanner');
+TextoBanner.innerHTML = '<a>EXISTING CUSTOMERS</a> <a style="text-decoration: underline;">LOG IN HERE</a>';
